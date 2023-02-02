5a70a1fca8858b24017aaa87a88c0322c6fbfb1e
youwol-guillaume <greinisch@youwol.com>
2023-02-02 15:17:44 +0100

    🔧 sync. config files w/ pipeline TS

--- src/auto-generated.ts
+++ src/auto-generated.ts
@@ -2,7 +2,7 @@
 const runTimeDependencies = {
     "externals": {
         "@youwol/cdn-client": "^1.0.10",
-        "@youwol/pyodide-helpers": "^0.1.1",
+        "@youwol/pyodide-helpers": "^0.1.2",
         "rxjs": "^6.5.5"
     },
     "includedInBundle": {}
@@ -47,8 +47,7 @@ const exportedSymbols = {
     }
 }
 
-// eslint-disable-next-line @typescript-eslint/ban-types -- allow to allow no secondary entries
-const mainEntry : Object = {
+const mainEntry : {entryFile: string,loadDependencies:string[]} = {
     "entryFile": "./index.ts",
     "loadDependencies": [
         "@youwol/cdn-client",
@@ -57,8 +56,8 @@ const mainEntry : Object = {
     ]
 }
 
-// eslint-disable-next-line @typescript-eslint/ban-types -- allow to allow no secondary entries
-const secondaryEntries : Object = {}
+const secondaryEntries : {[k:string]:{entryFile: string, name: string, loadDependencies:string[]}}= {}
+
 const entries = {
      '@youwol/pyodide-cdn-package-template': './index.ts',
     ...Object.values(secondaryEntries).reduce( (acc,e) => ({...acc, [`@youwol/pyodide-cdn-package-template/${e.name}`]:e.entryFile}), {})
@@ -66,7 +65,7 @@ const entries = {
 export const setup = {
     name:'@youwol/pyodide-cdn-package-template',
         assetId:'QHlvdXdvbC9weW9kaWRlLWNkbi1wYWNrYWdlLXRlbXBsYXRl',
-    version:'0.1.0',
+    version:'0.1.1-wip',
     shortDescription:"Template to publish pyodide project as a module in the YouWol's CDN.",
     developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/pyodide-cdn-package-template',
     npmPackage:'https://www.npmjs.com/package/@youwol/pyodide-cdn-package-template',
@@ -77,16 +76,20 @@ export const setup = {
     externals,
     exportedSymbols,
     entries,
+    secondaryEntries,
     getDependencySymbolExported: (module:string) => {
         return `${exportedSymbols[module].exportedSymbol}_APIv${exportedSymbols[module].apiKey}`
     },
 
-    installMainModule: ({cdnClient, installParameters}:{cdnClient, installParameters?}) => {
+    installMainModule: ({cdnClient, installParameters}:{
+        cdnClient:{install:(unknown) => Promise<Window>},
+        installParameters?
+    }) => {
         const parameters = installParameters || {}
         const scripts = parameters.scripts || []
         const modules = [
             ...(parameters.modules || []),
-            ...mainEntry['loadDependencies'].map( d => `${d}#${runTimeDependencies.externals[d]}`)
+            ...mainEntry.loadDependencies.map( d => `${d}#${runTimeDependencies.externals[d]}`)
         ]
         return cdnClient.install({
             ...parameters,
@@ -96,20 +99,24 @@ export const setup = {
             return window[`@youwol/pyodide-cdn-package-template_APIv01`]
         })
     },
-    installAuxiliaryModule: ({name, cdnClient, installParameters}:{name: string, cdnClient, installParameters?}) => {
+    installAuxiliaryModule: ({name, cdnClient, installParameters}:{
+        name: string,
+        cdnClient:{install:(unknown) => Promise<Window>},
+        installParameters?
+    }) => {
         const entry = secondaryEntries[name]
+        if(!entry){
+            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
+        }
         const parameters = installParameters || {}
         const scripts = [
             ...(parameters.scripts || []),
-            `@youwol/pyodide-cdn-package-template#0.1.0~dist/@youwol/pyodide-cdn-package-template/${entry.name}.js`
+            `@youwol/pyodide-cdn-package-template#0.1.1-wip~dist/@youwol/pyodide-cdn-package-template/${entry.name}.js`
         ]
         const modules = [
             ...(parameters.modules || []),
             ...entry.loadDependencies.map( d => `${d}#${runTimeDependencies.externals[d]}`)
         ]
-        if(!entry){
-            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
-        }
         return cdnClient.install({
             ...parameters,
             modules,
@@ -117,5 +124,13 @@ export const setup = {
         }).then(() => {
             return window[`@youwol/pyodide-cdn-package-template/${entry.name}_APIv01`]
         })
+    },
+    getCdnDependencies(name?: string){
+        if(name && !secondaryEntries[name]){
+            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
+        }
+        const deps = name ? secondaryEntries[name].loadDependencies : mainEntry.loadDependencies
+
+        return deps.map( d => `${d}#${runTimeDependencies.externals[d]}`)
     }
 }
